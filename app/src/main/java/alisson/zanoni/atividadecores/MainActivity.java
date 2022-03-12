8ab89c8fe54027ac9b6337076ff7cd6568843fc7
Gabrielmkmk <gabriel.marianok@unesc.net>
2022-03-12 20:56:18 -0300

    Adicionado Dialog ao selecionar uma cor para confirmação, toast e preferences para manter a cor ao fechar o app.

--- app/src/main/java/alisson/zanoni/atividadecores/MainActivity.java
+++ app/src/main/java/alisson/zanoni/atividadecores/MainActivity.java
@@ -1,96 +1,101 @@
 package alisson.zanoni.atividadecores;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.View;
+import android.preference.PreferenceManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private LinearLayout layoutBlack,layoutBlue,layoutRed,layoutGreen,layoutYellow;
-    private Button btnResetar;
+    boolean corSelecionada = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnResetar =findViewById(R.id.btnResetar);
-        layoutBlack =findViewById(R.id.layoutBlack);
-        layoutRed =findViewById(R.id.layoutRed);
-        layoutGreen =findViewById(R.id.layoutGreen);
-        layoutBlue =findViewById(R.id.layoutBlue);
-        layoutYellow =findViewById(R.id.layoutYellow);
+        Button btnResetar = findViewById(R.id.btnResetar);
+        layoutBlack = findViewById(R.id.layoutBlack);
+        layoutRed = findViewById(R.id.layoutRed);
+        layoutGreen = findViewById(R.id.layoutGreen);
+        layoutBlue = findViewById(R.id.layoutBlue);
+        layoutYellow = findViewById(R.id.layoutYellow);
 
 
-        layoutBlack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                layoutBlack.setBackgroundColor(Color.parseColor("black"));
-                layoutRed.setBackgroundColor(Color.parseColor("black"));
-                layoutGreen.setBackgroundColor(Color.parseColor("black"));
-                layoutBlue.setBackgroundColor(Color.parseColor("black"));
-                layoutYellow.setBackgroundColor(Color.parseColor("black"));
-            }
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        int preferenciaBackground = preferences.getInt("Cor Background", 0);
+
+        if(preferenciaBackground!=0){
+            changeBackgroundColor(preferenciaBackground);
+        }
+
+        layoutBlack.setOnClickListener(view -> abrirDialogo(((ColorDrawable)view.getBackground()).getColor()));
+
+        layoutRed.setOnClickListener(view -> abrirDialogo(((ColorDrawable)view.getBackground()).getColor()));
+
+        layoutGreen.setOnClickListener(view -> abrirDialogo(((ColorDrawable)view.getBackground()).getColor()));
+
+        layoutBlue.setOnClickListener(view -> abrirDialogo(((ColorDrawable)view.getBackground()).getColor()));
+
+        layoutYellow.setOnClickListener(view -> abrirDialogo(((ColorDrawable)view.getBackground()).getColor()));
+
+        btnResetar.setOnClickListener(view -> {
+            initialColor();
+            corSelecionada = false;
         });
+    }
 
-        layoutRed.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                layoutBlack.setBackgroundColor(Color.parseColor("red"));
-                layoutRed.setBackgroundColor(Color.parseColor("red"));
-                layoutGreen.setBackgroundColor(Color.parseColor("red"));
-                layoutBlue.setBackgroundColor(Color.parseColor("red"));
-                layoutYellow.setBackgroundColor(Color.parseColor("Red"));
-            }
-        });
+    public void changeBackgroundColor(int color){
+        layoutBlack.setBackgroundColor(color);
+        layoutRed.setBackgroundColor(color);
+        layoutGreen.setBackgroundColor(color);
+        layoutBlue.setBackgroundColor(color);
+        layoutYellow.setBackgroundColor(color);
+    }
 
-        layoutGreen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                layoutBlack.setBackgroundColor(Color.parseColor("green"));
-                layoutRed.setBackgroundColor(Color.parseColor("green"));
-                layoutGreen.setBackgroundColor(Color.parseColor("green"));
-                layoutBlue.setBackgroundColor(Color.parseColor("green"));
-                layoutYellow.setBackgroundColor(Color.parseColor("green"));
-            }
-        });
+    public void initialColor(){
+        layoutBlack.setBackgroundColor(Color.parseColor("Black"));
+        layoutRed.setBackgroundColor(Color.parseColor("Red"));
+        layoutGreen.setBackgroundColor(Color.parseColor("Green"));
+        layoutBlue.setBackgroundColor(Color.parseColor("Blue"));
+        layoutYellow.setBackgroundColor(Color.parseColor("Yellow"));
+    }
 
-        layoutBlue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                layoutBlack.setBackgroundColor(Color.parseColor("blue"));
-                layoutRed.setBackgroundColor(Color.parseColor("blue"));
-                layoutGreen.setBackgroundColor(Color.parseColor("blue"));
-                layoutBlue.setBackgroundColor(Color.parseColor("blue"));
-                layoutYellow.setBackgroundColor(Color.parseColor("blue"));
-            }
-        });
+    public void changePreferences(int color){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        SharedPreferences.Editor edit = preferences.edit();
+        edit.putInt("Cor Background", color);
+        edit.apply();
+    }
 
-        layoutYellow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                layoutBlack.setBackgroundColor(Color.parseColor("yellow"));
-                layoutRed.setBackgroundColor(Color.parseColor("yellow"));
-                layoutGreen.setBackgroundColor(Color.parseColor("yellow"));
-                layoutBlue.setBackgroundColor(Color.parseColor("yellow"));
-                layoutYellow.setBackgroundColor(Color.parseColor("yellow"));
-            }
-        });
+    public void abrirDialogo(int color){
+        if(!corSelecionada){
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
 
-        btnResetar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                layoutBlack.setBackgroundColor(Color.parseColor("Black"));
-                layoutRed.setBackgroundColor(Color.parseColor("Red"));
-                layoutGreen.setBackgroundColor(Color.parseColor("Green"));
-                layoutBlue.setBackgroundColor(Color.parseColor("Blue"));
-                layoutYellow.setBackgroundColor(Color.parseColor("Yellow"));
-            }
-        });
+            dialog.setTitle("Trocar Cor");
+            dialog.setMessage("Você deseja trocar todas as cores?");
 
+            dialog.setCancelable(false);
+
+            dialog.setIcon(android.R.drawable.ic_input_add);
+
+            dialog.setPositiveButton("Sim", (dialogInterface, i) -> {
+                changePreferences(color);
+                changeBackgroundColor(color);
+                corSelecionada = true;
+            });
+            dialog.setNegativeButton("Não", (dialogInterface, i) -> Toast.makeText(getApplicationContext(), "Operação cancelada", Toast.LENGTH_SHORT).show());
+
+            dialog.create();
+            dialog.show();
+        }
     }
 }
